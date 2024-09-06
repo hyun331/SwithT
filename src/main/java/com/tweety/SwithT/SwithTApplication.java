8d7cb5503e0d3eec86e8eefc8af527ec71a89265
Mincastle <kcm1042@naver.com>
2024-09-06 17:39:58 +0900

    init

---
+++ src/main/java/com/tweety/SwithT/SwithTApplication.java
@@ -0,0 +1,13 @@
+package com.tweety.SwithT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwithTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwithTApplication.class, args);
+	}
+
+}
