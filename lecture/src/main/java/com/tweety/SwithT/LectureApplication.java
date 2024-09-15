07029c988b0110115869e51b83a797836b3b522a
jasonhwang <junghahwang0601@gmail.com>
2024-09-15 15:40:04 +0900

    Fix: MSA 구축(서버 최종 수정)

--- lecture/src/main/java/com/tweety/SwithT/LectureApplication.java
+++ lecture/src/main/java/com/tweety/SwithT/LectureApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class SpeechApplication {
+public class LectureApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpeechApplication.class, args);
+		SpringApplication.run(LectureApplication.class, args);
 	}
 
 }
--- payment/src/main/java/com/tweety/SwithT/PaymentApplication.java
+++ payment/src/main/java/com/tweety/SwithT/PaymentApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class SwithtChatApplication {
+public class PaymentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SwithtChatApplication.class, args);
+		SpringApplication.run(PaymentApplication.class, args);
 	}
 
 }
