881d3c5d246f865d9d4dc388af2f3e4c04efb6a5
Mincastle <kcm1042@naver.com>
2024-09-20 00:43:49 +0900

    feat:MemberListResdto 추가

---
+++ member/src/main/java/com/tweety/SwithT/member/dto/MemberListResDto.java
@@ -0,0 +1,29 @@
+package com.tweety.SwithT.member.dto;
+
+import com.tweety.SwithT.member.domain.Gender;
+import com.tweety.SwithT.member.domain.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberListResDto {
+
+    //회원 리스트 조회
+    private Long id;
+    private String email;
+    private String nickName;
+    private String phoneNumber;
+    private String address;
+    private BigDecimal avgScore;
+    private Gender gender;
+
+
+
+}
