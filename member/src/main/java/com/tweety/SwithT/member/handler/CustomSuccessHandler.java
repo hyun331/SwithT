ce2908817c9eb55d8919c37d90fc62814559ff3f
Mincastle <kcm1042@naver.com>
2024-10-02 12:50:00 +0900

    feat: 쿼리파라미터형식 성공

--- member/src/main/java/com/tweety/SwithT/member/handler/CustomSuccessHandler.java
+++ member/src/main/java/com/tweety/SwithT/member/handler/CustomSuccessHandler.java
@@ -1,24 +1,19 @@
 package com.tweety.SwithT.member.handler;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tweety.SwithT.common.auth.JwtTokenProvider;
-import com.tweety.SwithT.common.dto.CommonResDto;
 import com.tweety.SwithT.common.service.RedisService;
 import com.tweety.SwithT.member.domain.Member;
 import com.tweety.SwithT.member.repository.MemberRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 @Component
@@ -38,36 +33,34 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
+        // 인증된 사용자 정보를 가져옵니다.
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         String email = oAuth2User.getAttribute("email");
 
+        // 회원 정보를 데이터베이스에서 조회합니다.
+        Optional<Member> member = memberRepository.findByEmail(email);
 
-        Optional<Member> member = memberRepository.findByEmail(email); // 회원 ID 조회
-        String name = member.get().getName();
-//        System.out.println("핸들러!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-//        System.out.println(member.get().getId());
-//        System.out.println(member.get().getName());
-//        System.out.println(member.get().getEmail());
-//        System.out.println(member.get().getRole());
+        // 회원이 존재하는지 확인하고, 정보 추출
+        if (member.isPresent()) {
+            String name = member.get().getName();
+            Long memberId = member.get().getId();
 
-        // 토큰 생성
-        String jwtToken = jwtTokenProvider.createToken(String.valueOf(member.get().getId()), email, String.valueOf(member.get().getRole()), name);
+            // JWT 토큰 생성
+            String jwtToken = jwtTokenProvider.createToken(String.valueOf(memberId), email, String.valueOf(member.get().getRole()), name);
 
-        System.out.println("토큰은!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        System.out.println(jwtToken);
+            System.out.println(jwtToken);
+            System.out.println(memberId);
+            // 리디렉션 URL에 토큰과 회원 정보를 쿼리 파라미터로 추가
+            String redirectUrl = REDIRECT_URL + "?token=" + jwtToken + "&memberId=" + memberId;
 
-        // 응답 정보 설정
-        Map<String, Object> loginInfo = new HashMap<>();
-        loginInfo.put("id", member.get().getId());
-        loginInfo.put("token", jwtToken);
-
-        CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "Login is successful", loginInfo);
-
-        // JSON 형태로 응답 반환
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().write(new ObjectMapper().writeValueAsString(commonResDto));
+            // 리디렉션
+            response.sendRedirect(redirectUrl);
+        } else {
+            // 회원 정보가 없을 경우의 처리 (예: 에러 응답)
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "User not found");
+        }
     }
+
 
 //    @Override
 //    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException{
