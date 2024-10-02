3687f34413d807e9aef04b229e13c5e8a4e2a32f
Mincastle <kcm1042@naver.com>
2024-10-02 12:31:04 +0900

    feat: 쿼리파라미터 형식으로 수정하기전 커밋

--- member/src/main/java/com/tweety/SwithT/member/handler/CustomSuccessHandler.java
+++ member/src/main/java/com/tweety/SwithT/member/handler/CustomSuccessHandler.java
@@ -1,19 +1,24 @@
 package com.tweety.SwithT.member.handler;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tweety.SwithT.common.auth.JwtTokenProvider;
+import com.tweety.SwithT.common.dto.CommonResDto;
 import com.tweety.SwithT.common.service.RedisService;
 import com.tweety.SwithT.member.domain.Member;
 import com.tweety.SwithT.member.repository.MemberRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Component
@@ -36,35 +41,32 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         String email = oAuth2User.getAttribute("email");
 
+
         Optional<Member> member = memberRepository.findByEmail(email); // 회원 ID 조회
-//
-        System.out.println("핸들러!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        System.out.println(member.get().getId());
-        System.out.println(member.get().getName());
-        System.out.println(member.get().getEmail());
-        System.out.println(member.get().getRole());
+        String name = member.get().getName();
+//        System.out.println("핸들러!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+//        System.out.println(member.get().getId());
+//        System.out.println(member.get().getName());
+//        System.out.println(member.get().getEmail());
+//        System.out.println(member.get().getRole());
 
+        // 토큰 생성
+        String jwtToken = jwtTokenProvider.createToken(String.valueOf(member.get().getId()), email, String.valueOf(member.get().getRole()), name);
 
-//        // 토큰 생성
-//        String jwtToken = jwtTokenProvider.createToken(String.valueOf(member.get().getId()), email, "ROLE_USER", name);
-//        String refreshToken = jwtTokenProvider.createRefreshToken(String.valueOf(memberId), email, "ROLE_USER", name);
+        System.out.println("토큰은!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println(jwtToken);
 
-//        // Redis에 Refresh Token 저장
-//        redisTemplate.opsForValue().set(email, refreshToken, 240, TimeUnit.HOURS);
+        // 응답 정보 설정
+        Map<String, Object> loginInfo = new HashMap<>();
+        loginInfo.put("id", member.get().getId());
+        loginInfo.put("token", jwtToken);
 
-//        // 응답 정보 설정
-//        Map<String, Object> loginInfo = new HashMap<>();
-//        loginInfo.put("id", memberId);
-//        loginInfo.put("token", jwtToken);
-//        loginInfo.put("refreshToken", refreshToken);
-//        loginInfo.put("redirectUri", "http://localhost:8082/additionalInfo"); // 추가정보 입력 URI
-//
-//        CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "Login is successful", loginInfo);
-//
-//        // JSON 형태로 응답 반환
-//        response.setContentType("application/json");
-//        response.setCharacterEncoding("UTF-8");
-//        response.getWriter().write(new ObjectMapper().writeValueAsString(commonResDto));
+        CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "Login is successful", loginInfo);
+
+        // JSON 형태로 응답 반환
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(new ObjectMapper().writeValueAsString(commonResDto));
     }
 
 //    @Override
