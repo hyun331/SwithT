6ef37d691b6f3d86039653aa9a51c71fcacdbb2a
JasonHwang <junghahwang0601@gmail.com>
2024-10-15 17:01:54 +0900

    fix: 강의만 스케줄에 추가하던 문제 수정

--- member/src/main/java/com/tweety/SwithT/scheduler/service/SchedulerService.java
+++ member/src/main/java/com/tweety/SwithT/scheduler/service/SchedulerService.java
@@ -61,34 +61,32 @@ public class SchedulerService {
 
             // 각 GroupTimeResDto를 처리하는 로직
             for (GroupTimeResDto groupTimeResDto : groupTimeResDtos) {
-                if (groupTimeResDto.getLectureType().equals("LECTURE")) {
-                    LocalDate startDate = LocalDate.parse(groupTimeResDto.getStartDate());
-                    LocalDate endDate = LocalDate.parse(groupTimeResDto.getEndDate());
+                LocalDate startDate = LocalDate.parse(groupTimeResDto.getStartDate());
+                LocalDate endDate = LocalDate.parse(groupTimeResDto.getEndDate());
 
-                    // GroupTimeResDto에 있는 요일 정보 (MON, TUE 등)를 DayOfWeek로 변환
-                    DayOfWeek lectureDay = DayOfWeek.valueOf(groupTimeResDto.getLectureDay().toUpperCase());
+                // GroupTimeResDto에 있는 요일 정보 (MON, TUE 등)를 DayOfWeek로 변환
+                DayOfWeek lectureDay = DayOfWeek.valueOf(groupTimeResDto.getLectureDay().toUpperCase());
 
-                    List<Scheduler> schedulers = new ArrayList<>();  // 스케줄러 리스트를 생성
+                List<Scheduler> schedulers = new ArrayList<>();  // 스케줄러 리스트를 생성
 
-                    // startDate부터 endDate까지 날짜를 반복하면서 요일을 확인
-                    for (LocalDate date = startDate; !date.isAfter(endDate); date = date.plusDays(1)) {
-                        if (date.getDayOfWeek().equals(lectureDay)) {
-                            // 요일이 일치하면 스케줄러 생성
-                            Scheduler scheduler = Scheduler.builder()
-                                    .title(groupTimeResDto.getSchedulerTitle())
-                                    .schedulerDate(date)
-                                    .schedulerTime(LocalTime.parse(groupTimeResDto.getStartTime()))  // 강의 시작 시간 설정
-                                    .content(groupTimeResDto.getSchedulerTitle() + "가 있는 날입니다.")
-                                    .alertYn(groupTimeResDto.getAlertYn())
-                                    .member(member)
-                                    .lectureGroupId(groupTimeResDto.getLectureGroupId())
-                                    .build();
+                // startDate부터 endDate까지 날짜를 반복하면서 요일을 확인
+                for (LocalDate date = startDate; !date.isAfter(endDate); date = date.plusDays(1)) {
+                    if (date.getDayOfWeek().equals(lectureDay)) {
+                        // 요일이 일치하면 스케줄러 생성
+                        Scheduler scheduler = Scheduler.builder()
+                                .title(groupTimeResDto.getSchedulerTitle())
+                                .schedulerDate(date)
+                                .schedulerTime(LocalTime.parse(groupTimeResDto.getStartTime()))  // 강의 시작 시간 설정
+                                .content(groupTimeResDto.getSchedulerTitle() + "가 있는 날입니다.")
+                                .alertYn(groupTimeResDto.getAlertYn())
+                                .member(member)
+                                .lectureGroupId(groupTimeResDto.getLectureGroupId())
+                                .build();
 
-                            schedulers.add(scheduler);
-                        }
+                        schedulers.add(scheduler);
                     }
-                    schedulerRepository.saveAll(schedulers);
                 }
+                schedulerRepository.saveAll(schedulers);
             }
         } catch (JsonProcessingException e) {
             System.err.println("Kafka 메시지 변환 중 오류 발생: " + e.getMessage());
