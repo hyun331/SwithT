1cbfcdb7771e6a2fb32a5fce8fee4ccd2f08eca2
Mincastle <kcm1042@naver.com>
2024-10-29 19:21:09 +0900

    feat:인그레스컨트롤러 설정

--- member/src/main/java/com/tweety/SwithT/member/handler/CustomSuccessHandler.java
+++ member/src/main/java/com/tweety/SwithT/member/handler/CustomSuccessHandler.java
@@ -19,7 +19,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private final JwtTokenProvider jwtTokenProvider;
     private final MemberRepository memberRepository;
     private final String REDIRECT_URL = "https://www.switht.co.kr/member/explain";
-    private final String REDIRECT_URL_EXIST = "https://www.switht.co.kr/";//테스트를 위해서 임시로 뒀음.
+    private final String REDIRECT_URL_EXIST = "https://www.switht.co.kr/loginSuccess";//테스트를 위해서 임시로 뒀음.
 
     public CustomSuccessHandler(JwtTokenProvider jwtTokenProvider, MemberRepository memberRepository) {
         this.jwtTokenProvider = jwtTokenProvider;
@@ -60,12 +60,12 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
             refreshTokenCookie.setMaxAge(60 * 60);  // 1시간 유지
             response.addCookie(refreshTokenCookie);
 
+            // memberId도 쿠키에 저장
             Cookie memberCookie = new Cookie("memberId", String.valueOf(memberId));
-            memberCookie.setHttpOnly(false); // JavaScript에서 접근 가능
+            memberCookie.setHttpOnly(false);
+//            memberCookie.setSecure(true);  // HTTPS 환경에서만 전송
             memberCookie.setPath("/");
-            memberCookie.setDomain(".switht.co.kr");  // 상위 도메인으로 설정하여 모든 서브도메인에서 접근 가능하게 설정
-            memberCookie.setMaxAge(60 * 60); // 1시간 유지
-            memberCookie.setSecure(request.isSecure()); // HTTPS 환경에서만 true
+            memberCookie.setMaxAge(60 * 60 );  // 1시간 유지
             response.addCookie(memberCookie);
 
             // phoneNumber 필드가 null인지 확인
